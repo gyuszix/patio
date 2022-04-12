82beb45bc2695574242e9a88f58b134519ca90d3
gyuszix <gyuszix@gmail.com>
2022-04-12 22:55:59 +0200

    something

--- main.py
+++ main.py
@@ -1,67 +1,30 @@
-# Conway's Game of Life
-import random, time, copy
-WIDTH = 30
-HEIGHT = 10
+# exercise: coin flip streaks
+import random
 
-# Create a list of list for the cells:
-nextCells = []
-for x in range(WIDTH):
-    column = [] # Create a new column.
-    for y in range(HEIGHT):
-        if random.randint(0, 1) == 0:
-            column.append('#') # Add a living cell.
+#variable declaration
+
+numberOfStreaks = 0
+CoinFlip = []
+streak = 0
+
+for experimentNumber in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+    for i in range(100):
+        CoinFlip.append(random.randint(0,1))
+    #does not matter if it is 0 or 1, H or T, peas or lentils. I am going to check if there is multiple 0 or 1 in a row
+
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+    for i in range(len(CoinFlip)):
+        if i==0:
+            pass
+        elif CoinFlip[i] == CoinFlip[i-1]:  #checks if current list item is the same as before
+            streak += 1
         else:
-            column.append(' ') # Add a dead cell.
-    nextCells.append(column) # nextCells is a list of column lists.
+            streak = 0
 
-while True: # Main program loop.
-    print('\n\n\n\n\n') # Separate each step with newlines.
-    currentCells = copy.deepcopy(nextCells)
+        if streak == 6:
+            numberOfStreaks += 1
 
-    # Print currentCells on the screen:
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            print(currentCells[x][y], end='') # Print the # or space.
-        print() # Print a newline at the end of the row.
+    CoinFlip = []
 
-    # Calculate the next step's cells based on current step's cells:
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            # Get neighboring coordinates:
-            # `% WIDTH` ensures leftCoord is always between 0 and WIDTH - 1
-            leftCoord  = (x - 1) % WIDTH
-            rightCoord = (x + 1) % WIDTH
-            aboveCoord = (y - 1) % HEIGHT
-            belowCoord = (y + 1) % HEIGHT
-
-            # Count number of living neighbors:
-            numNeighbors = 0
-            if currentCells[leftCoord][aboveCoord] == '#':
-                numNeighbors += 1 # Top-left neighbor is alive.
-            if currentCells[x][aboveCoord] == '#':
-                numNeighbors += 1 # Top neighbor is alive.
-            if currentCells[rightCoord][aboveCoord] == '#':
-                numNeighbors += 1 # Top-right neighbor is alive.
-            if currentCells[leftCoord][y] == '#':
-                numNeighbors += 1 # Left neighbor is alive.
-            if currentCells[rightCoord][y] == '#':
-                numNeighbors += 1 # Right neighbor is alive.
-            if currentCells[leftCoord][belowCoord] == '#':
-                numNeighbors += 1 # Bottom-left neighbor is alive.
-            if currentCells[x][belowCoord] == '#':
-                numNeighbors += 1 # Bottom neighbor is alive.
-            if currentCells[rightCoord][belowCoord] == '#':
-                numNeighbors += 1 # Bottom-right neighbor is alive.
-
-            # Set cell based on Conway's Game of Life rules:
-            if currentCells[x][y] == '#' and (numNeighbors == 2 or
-numNeighbors == 3):
-                # Living cells with 2 or 3 neighbors stay alive:
-                nextCells[x][y] = '#'
-            elif currentCells[x][y] == ' ' and numNeighbors == 3:
-                # Dead cells with 3 neighbors become alive:
-                nextCells[x][y] = '#'
-            else:
-                # Everything else dies or stays dead:
-                nextCells[x][y] = ' '
-    time.sleep(1) # Add a 1-second pause to reduce flickering.
+print('Chance of streak: %s%%' % (numberOfStreaks / (100*10000)))
