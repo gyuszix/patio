fcb645e2a5b03ed2f76199de8fa3cfc373943312
gyuszix <gyuszix@gmail.com>
2022-04-28 23:00:11 +0200

    discovering classes with the help of Corey working through chapter 6 of ATBS

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
-import pprint
+def odd_or_even():
+    user_input = input('please input your number\n')
+    if user_input.isdigit():
+        if int(user_input) % 2 == 0:
+            return user_input + ' is even'
+        elif int(user_input) % 2 != 0:
+            return user_input + ' is odd'
 
 
-# birthday = {'Gyula': 'Feb 06',
-#             'Gyula': 'Feb 07',
-#             'Misha': 'Feb 07',
-#             'Nao':   'Feb 08'}
-#
-# while True:
-#         name = input('Enter a name or press nothing to stop the program\n')
-#         if name == '':
-#                 break
-#
-#         if name in birthday:
-#                 print( '{} is the birthday of {}'.format(birthday[name], name))
-#         else:
-#                 print('That is not in the database\n When is your birthday?')
-#                 bday = input()
-#                 birthday[name] = bday
-#                 print('birthday database now updated:' + str(birthday))
-
-#
-# print(list(birthday))
-# print(birthday['Gyula'])
-# print('Gyula' in birthday)
-# print('Gyula' in birthday.values())
-
-
+print(odd_or_even())
