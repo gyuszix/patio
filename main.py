acc808fac1c87bbaa09668d129d411a2fa787b2d
gyuszix <gyuszix@gmail.com>
2023-01-06 17:44:57 +0100

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -1,31 +1,85 @@
-def valid_parentheses(string):
-    #this function is supposed to see if the parantheses all have correct ends and beginnengs
-    arr = []
-    for char in string:
-        if char == '(' or char == ')':
-            arr.append(char)
+# class Rectangle:
+#     def __init__(self,length, width):
+#         self.length = length
+#         self.width = width
+#
+#     def area(self):
+#         return self.length * self.width
+#
+#     def parameter(self):
+#         return self.length * 2 + self.width * 2
+#
+#
+# class Square:
+#     def __init__(self, length):
+#         self.length = length
+#
+#     def area(self):
+#         return self.length ** 2
+#
+#     def parameter(self):
+#         return self.length * 4
+#
+#
+# first_rectangle = Rectangle(5,4)
+# first_square = Square(5)
+#
+# print(first_rectangle.parameter())
+# print(first_rectangle.area())
+#
+# print(first_square.parameter())
+# print(first_square.area())
 
-    if len(arr) % 2 == 0 and len(arr) != 0:
-        arr_1 = arr[0:int((len(arr)/2))]
-        arr_2 = arr[int((len(arr))/2):int(len(arr))]
-        arr_2.reverse()
+#so it seems that doing it this way as seen above is redundant because when it doesn't actually represent the
+#relationship between the two shapes. A better way of writing this would be just to inherit and use the super method
 
-        for i in range(len(arr_1)):
-            if arr_1[i] == arr_2[i]:
-                print('f')
-                return False
-            if arr_1[i] == ')':
-                print('f')
-                return False
-        print('t')
-        return True
-    print('f')
-    return False
+class Rectangle:
+    def __init__(self,length, width):
+        self.length = length
+        self.width = width
 
-valid_parentheses('(( )) ()())')
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return self.length * 2 + self.width * 2
 
 
-# needs refining
-# https://www.codewars.com/kata/52774a314c2333f0a7000688/train/python
-# a good idea would be to make two lists and count one by one how many are missing
-# also on top of that idea on top just make sure to have a flag like 'begins with ( == yes
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
+
+
+class Cube(Square):
+    def surface_area(self):
+        face_area = super().area()
+        return face_area * self.length
+
+    def volume(self):
+        face_area = super().area()
+        return  face_area * self.length
+
+
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
+
+class RightPyramid(Triangle, Square):
+    def __init__(self, base, slant_height):
+        self.base = base
+        self.slant_height = slant_height
+        super().__init__(self.base)
+
+    def area(self):
+        base_area = super().area()
+        perimeter = super().perimeter
+        return 0.5 * perimeter * self.slant_height + base_area
+
+    def area_2(self):
+        base_area = super().area()
+        triangle_area = super().tri
